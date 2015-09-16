ff56db5242cec023176f5ae963d185a9e66e6a7d
sabas <sabas88@gmail.com>
2015-09-16 14:46:15 +0200

    Depend directly on fontnik

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 genfontgl
 -----
-A simple command line tool to generate fonts for Mapbox GL via fontmachine.
+A simple command line tool to generate fonts for Mapbox GL via fontnik without gzipping the result.
 
 Usage: ```genfontgl OpenSans-Regular.ttf```
+
+Based on:
+* [Fontmachine](https://github.com/mapbox/fontmachine)
+* [build-glyphs](https://github.com/mapbox/node-fontnik/blob/master/bin/build-glyphs)
--- index.js
+++ index.js
@@ -1,37 +1,48 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
-var fm = require('fontmachine');
+var fontnik = require('fontnik');
+var queue = require('queue-async');
 
-	try {
-        var fname=process.argv[2];
-		var data = fs.readFileSync(fname);
-		console.log('Process '+fname);
-        fm.makeGlyphs({font: data, fileType: 'ttf'}, function p(error,result){
-            var folder=result.name;
+    try {
+        var fname = process.argv[2];
+	
+	var fontstack = fs.readFileSync(fname);
+        console.log('Process '+fname);
+        
+        var folder=fname.slice(0, -4).replace('-','');
+    } catch (e) {
+        console.error('error: could not read font '+e)
+        return;
+    }
 
-             if(!fs.existsSync(folder)){
-                 fs.mkdirSync(folder, 0766, function(err){
-                   if(err){ 
-                     console.log(err);
-                     response.send("ERROR! Can't make the directory! \n");
-                   }
-                 });   
-             }
+    if(!fs.existsSync(folder)){
+       fs.mkdirSync(folder, 0766, function(err){
+         if(err){ 
+            console.log(err);
+            response.send("ERROR! Can't make the directory! \n");
+         }
+       });   
+    }
 
-            for(var index in result.stack) { 
-                var font=result.stack[index].name;
-                var fontData=result.stack[index].data;
+     var q = queue(Math.max(4, require('os').cpus()));
+     var queue = [];
+     for (var i = 0; i < 65536; (i = i + 256)) {
+         q.defer(writeGlyphs, {
+            font: fontstack,
+            start: i,
+            end: Math.min(i + 255, 65535)
+         });
+     }
 
-                fs.writeFile(folder+'/'+font, fontData, function(err) {
-                    if(err) {
-                        return console.log(err);
-                    }
-                }); 
-            }
 
-        });
-	} catch (e) {
-		console.error('error: could not read font '+e)
-		return;
-	}
+function writeGlyphs(opts, done) {
+    fontnik.range(opts, function(err, zdata) {
+        if (err) {
+            console.warn(err.toString());
+            process.exit(1);
+        }
+        fs.writeFileSync(folder + '/' + opts.start + '-' + opts.end + '.pbf', zdata);
+        done();
+    });
+}
